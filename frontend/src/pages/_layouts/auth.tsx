4ee4544ee7cc34682fdaf0cf394d42adac14560e
JadeOweis <jade.m.oweis@gmail.com>
2024-09-14 18:26:44 -0300

    Criar estrutura e estilo da pagina de login

--- frontend/src/pages/_layouts/auth.tsx
+++ frontend/src/pages/_layouts/auth.tsx
@@ -1,11 +1,22 @@
+import { Table } from '@phosphor-icons/react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="min-h-screen md:grid md:grid-cols-2">
+      <header className="hidden h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground md:flex">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Table className="h-5 w-5" />
 
-      <main>
+          <span className="font-medium">diet.plan</span>
+        </div>
+        <footer>
+          Controle seu plano de dieta &copy; diet.plan 💪 -{' '}
+          {new Date().getFullYear()}
+        </footer>
+      </header>
+
+      <main className="flex flex-col items-center justify-center">
         <Outlet />
       </main>
     </div>
--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -1,10 +1,39 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="LogIn" />
-      <h1>SignIn</h1>
+      <section className="w-full max-w-md p-4 md:p-8">
+        <article className="flex w-full flex-col justify-center gap-6">
+          <div className="flex flex-col text-center md:gap-2">
+            <h1 className="text-xl font-semibold tracking-tighter md:text-2xl">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe seu plano de dieta pelo painel
+            </p>
+          </div>
+        </article>
+
+        <form className="flex flex-col gap-2 md:gap-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email" />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="password">Sua senha</Label>
+            <Input id="password" type="password" />
+          </div>
+
+          <Button type="submit">Acessar painel</Button>
+        </form>
+      </section>
     </>
   )
 }
