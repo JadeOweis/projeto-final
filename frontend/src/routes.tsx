8d5b39a173def559971cd3e45ceb8817ec184d0a
JadeOweis <jade.m.oweis@gmail.com>
2024-09-14 17:54:10 -0300

    Configurar layouts padrões das rotas

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,15 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Home } from './pages/app/home'
 import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
   },
   {
-    path: '/sign-in',
-    element: <SignIn />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+    ],
   },
 ])
